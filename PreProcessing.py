94677fc35720f5c3cde02ea354de6f76250a4405
yeimsf <yeimsf@gmail.com>
2022-02-12 16:02:39 +0200

    Finished Dataset Improvements

--- PreProcessing.py
+++ PreProcessing.py
@@ -3,30 +3,68 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+pd.options.mode.chained_assignment = None
 
 # Dataset Reading/Cleaning
-def Data_Reading(path):
-    data = pd.read_csv(path).dropna()
-    data_sus = data['total_points']
-    data.pop(['bps', 'team_x', 'kickoff_time', 'total_point'])
-    data['total_points'] = data_sus
-    return data
+def Data_Reading(ppath, tpath):
+    Players_Data = pd.read_csv(ppath, dtype={"name": str, "position": str, "team_x": str})
+    Teams_Data = pd.read_csv(tpath)
+    data_sus = Players_Data['total_points']
+    Players_Data.pop('total_points')
+    Players_Data = Players_Data.drop(['bps', 'team_x', 'kickoff_time'], axis=1)
+    Players_Data['total_points'] = data_sus
+    return Players_Data, Teams_Data
 
 # Dataset Encoding
 def Data_Encoding(data, cols):
-    return data
+    data_tmp = data
+    for col in cols:
+        lbl = LabelEncoder()
+        lbl.fit(data_tmp[col].values)
+        data_tmp[col] = lbl.transform(list(data_tmp[col].values))
+    return data_tmp
 
-# Dataset Seasons Splitting
-def Data_Seasons_Splitting():
-    return Dict17, Dict18, Dict19, Dict20, Dict21, Dict22
+# Dataset Seasons Splitting For Players Dataset
+def Players_Data_Seasons_Splitting(pdf):
+    pdf17 = pdf.loc[pdf['season_x'] == '2016-17']
+    pdf18 = pdf.loc[pdf['season_x'] == '2017-18']
+    pdf19 = pdf.loc[pdf['season_x'] == '2018-19']
+    pdf20 = pdf.loc[pdf['season_x'] == '2019-20']
+    pdf21 = pdf.loc[pdf['season_x'] == '2020-21']
+    pdf22 = pdf.loc[pdf['season_x'] == '2021-22']
+    return pdf17, pdf18, pdf19, pdf20, pdf21, pdf22
 
-# Dataset Initialization For Each Season
-def Data_Init_Seasons():
-    return Data17, Data18, Data19, Data20, Data21, Data22_
+# Dataset Seasons Splitting For Teams Dataset
+def Teams_Data_Seasons_Splitting(tdf):
+    tdf17 = tdf.loc[tdf['season'] == '2016-17']
+    tdf18 = tdf.loc[tdf['season'] == '2017-18']
+    tdf19 = tdf.loc[tdf['season'] == '2018-19']
+    tdf20 = tdf.loc[tdf['season'] == '2019-20']
+    tdf21 = tdf.loc[tdf['season'] == '2020-21']
+    tdf22 = tdf.loc[tdf['season'] == '2021-22']
+    return tdf17, tdf18, tdf19, tdf20, tdf21, tdf22
+
+# Teams Integration In Players Dataset
+def Teams_Data_Integration(pdf, tdf):
+    team = dict()
+    for i in range(len(tdf)):
+        team[tdf.iloc[i,1]] = tdf.iloc[i,2]
+    for i in team:
+        pdf.loc[pdf['opponent_team'] == i, 'opponent_team'] = team[i] 
+    return pdf
 
 # Dataset Sorting Based On Players' Names
-def Data_Sorting():
-    return data
+def Data_Sorting(pdf, cols):
+    pdf = pdf.sort_values(cols, ascending=True)
+    return pdf
 
-
+# Dataset Tidiness For Extracting Players/Teams Datasets Ready To Use After [Encoding Column opponent_team To Match All Seasons]
+def Data_Tidiness(pdf):
+    teams_names = pd.DataFrame()
+    teams_names = pdf['opponent_team']
+    pdf_encoded = Data_Encoding(pdf, ['opponent_team'])
+    tdf_encoded = pd.concat([pdf_encoded['opponent_team'], teams_names], axis=1).drop_duplicates().reset_index().drop('index', axis=1)
+    tdf_encoded.columns.values[0] = 'code'
+    tdf_encoded = Data_Sorting(tdf_encoded, ['code']).reset_index().drop('index', axis=1)
+    return pdf_encoded, tdf_encoded
 
--- Script.py
+++ Script.py
@@ -6,21 +6,32 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from PreProcessing import *
 
-data = DataReading("./cleaned_merged_seasons.csv") 
+# Data Reading
+pdata, tdata = Data_Reading("./cleaned_merged_seasons.csv", "./master_team_list.csv") 
 
+# Data Encoding For Columns: [position, was_home]
+pdata = Data_Encoding(pdata, ['position', 'was_home'])
+
+# Players' Data Splitting For Team Codes Management 
+pdf17, pdf18, pdf19, pdf20, pdf21, pdf22 = Players_Data_Seasons_Splitting(pdata)
+
+# Teams' Data Splitting For Team Codes Management
+tdf17, tdf18, tdf19, tdf20, tdf21, tdf22 = Teams_Data_Seasons_Splitting(tdata)
+
+# Teams Integration and Sorting According To Columns: [name, GW]
+for pdf, tdf in zip([pdf17, pdf18, pdf19, pdf20, pdf21, pdf22], [tdf17, tdf18, tdf19, tdf20, tdf21, tdf22]):
+    pdf = Teams_Data_Integration(pdf, tdf)
+    pdf = Data_Sorting(pdf,['name', 'GW'])
+
+# Data Concatenation and Tidiness For Extracting Ready To Use Datasets: pdf_encoded, tdf_encoded For Players and Teams 
+pdf = pd.concat([pdf17, pdf18, pdf19, pdf20, pdf21, pdf22])
+pdf_encoded, tdf_encoded =  Data_Tidiness(pdf)
 
 # Dataset Correlation Determination
-'''
-features = pd.concat([features1, features2], axis=1)
-res = data["total_points"]
-
-features.drop('kickoff_time', axis=1, inplace=True)
-
-corr = data.corr()
-top = corr.index[abs(corr['total_points']>0.1)]
-
+res = pdf['total_points']
+corr = pdf.corr()
+top = corr.index[abs(corr['total_points']>0.5)]
 plt.subplots(figsize=(12,8))
-top_corr = data[top].corr()
+top_corr = pdf[top].corr()
 sns.heatmap(top_corr, annot=True)
 plt.show()
-'''
